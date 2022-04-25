8879fd35c14f89b2c3bca0a1d0e26d60a858a5d5
Emre Erdem <fo.emre@gmail.com>
2022-04-26 00:08:13 +0300

    fix cmdline args

--- src/pseudocolor.cpp
+++ src/pseudocolor.cpp
@@ -3,13 +3,14 @@
 #include <../include/InputParser.hpp>
 int main(int argc, char **argv){
 
-    // InputParser input(argc, argv);
-    // const std::string &cMapText = input.getCmdOption("-m");
-    // int colorMap = std::stoi(cMapText.c_str());
-    // if(!input.cmdOptionExists("-m")){
-    //     std::cout << "Usage: " << std::endl << "-m NUMBER - Specify OpenCV colormap" << std::endl <<
-    //         "Reference: https://docs.opencv.org/4.x/d3/d50/group__imgproc__colormap.html" << std::endl;
-    // }
+    InputParser input(argc, argv);
+    if(!input.cmdOptionExists("-m") || argc < 2){
+        std::cout << "Usage: " << std::endl << "-m NUMBER - Specify OpenCV colormap" << std::endl <<
+            "Reference: https://docs.opencv.org/4.x/d3/d50/group__imgproc__colormap.html" << std::endl;
+        return 0;
+    }
+    const std::string &cMapText = input.getCmdOption("-m");
+    long colorMap = std::stol(cMapText);
     
     cv::Mat image;
     cv::Mat colorImage;
@@ -21,7 +22,7 @@ int main(int argc, char **argv){
     cap.set(cv::CAP_PROP_FRAME_HEIGHT, 720.0);
     while (true) {
         cap >> image;
-        cv::applyColorMap(image, colorImage, cv::COLORMAP_JET);
+        cv::applyColorMap(image, colorImage, colorMap);
         imshow("Display window", colorImage);
         cv::waitKey(25);
     }
